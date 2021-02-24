5b66a84f67449002a1d203aea2215bf20f313ce4
unknown <patryk.maternicki@gmail.com>
2021-02-24 14:33:23 +0100

    item styling

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,5 +1,9 @@
-const Item = ({src}) => (
-  <img src={src} />
+const Item = ({src, className = 'componentItem'}) => (
+  <div className={className}>
+    <figure className={`${className}__figure`}>
+      <img className={`${className}__img`} src={src} />
+    </figure>
+  </div>
 )
 
 export default Item
